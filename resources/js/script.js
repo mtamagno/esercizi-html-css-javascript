c2676841312a70592d642e74affec7548bb1ac02
FraEl11 <brainybakasaru@gmail.com>
2019-10-02 14:24:21 +0200

    m

--- resources/js/script.js
+++ resources/js/script.js
@@ -45,4 +45,12 @@ $(document).ready(function() {
      }
    });
 
+   /* Animations on scroll */
+   $('.js--wp-1').waypoint(function(direction){
+     $('js--wp-1').addClass('animated fadeIn');
+    }, {
+      offset: '50%'
+    });
+
+
 });
